25266fdada8355deb7474d901e08819959e919e8
Leander Druwel <79353375+leanderdruwel-skyline@users.noreply.github.com>
2023-11-30 17:12:19 +0100

    Add solution

--- SLC-GQIDS-AutomationLogScriptStats/SLC-GQIDS-AutomationLogScriptStats_1/SLC-GQIDS-AutomationLogScriptStats_1.cs
+++ SLC-GQIDS-AutomationLogScriptStats/SLC-GQIDS-AutomationLogScriptStats_1/SLC-GQIDS-AutomationLogScriptStats_1.cs
@@ -49,26 +49,26 @@ DATE		VERSION		AUTHOR			COMMENTS
 ****************************************************************************
 */
 
-using Skyline.DataMiner.Automation;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
-
 namespace SLC_GQIDS_AutomationLogScriptStats_1
 {
-    /// <summary>
-    /// Represents a DataMiner Automation script.
-    /// </summary>
-    public class Script
-    {
-        /// <summary>
-        /// The script entry point.
-        /// </summary>
-        /// <param name="engine">Link with SLAutomation process.</param>
-        public void Run(IEngine engine)
-        {
-
-        }
-    }
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+	using Skyline.DataMiner.Automation;
+	
+	/// <summary>
+	/// Represents a DataMiner Automation script.
+	/// </summary>
+	public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+	
+		}
+	}
 }
